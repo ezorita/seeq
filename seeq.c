9dc336ba632844271babe76ded515eaa8c5724ec
Eduard Valera i Zorita <eduardvalera@gmail.com>
2014-07-14 22:10:19 +0200

    Started automata algorithm.

--- seeq.c
+++ seeq.c
@@ -18,17 +18,33 @@ int main(int argc, char *argv[])
    
    char * keys;
    int    wlen = parse(expression, keys);
+   int    nnfa = 2*tau + 1;
 
-   nfa_t * nfa = malloc ((2*tau+1)*sizeof(nfa_t));
-   for (int i = 0; i < 2*tau+1; i++) {
-      nfa[i].rows = tau + 1;
-      nfa[i].cols = wlen;
-      nfa[i].exp  = keys;
-      nfa[i].act  = calloc(nfa.rows*nfa.cols, sizeof(int));
+   nfa_t * nfa = malloc (nnfa*sizeof(nfa_t));
+   for (int i = 0; i < nnfa; i++) {
+      nfa[i].rows  = tau + 1;
+      nfa[i].cols  = wlen + 1;
+      nfa[i].exp   = keys;
+      nfa[i].act   = calloc(nfa.rows*nfa.cols, sizeof(int));
+      nfa[i].stack = new_stack(INITIAL_STACK_SIZE);
+      nfa[i].temp  = new_stack(INITIAL_STACK_SIZE);
+      nfa[i].mbuf  = malloc(sizeof(mbuf_t) + nnfa*sizeof(int));
+      nfa[i].mbuf->p = nnfa - i;
    }
 
    // Read and update
-   while (readline(
+   char * line = malloc(INITIAL_LINE_SIZE*sizeof(char));
+   size_t size;
+   int status = 1;
+   while (getline(&line, &size, in) != -1) {
+      line[size] = 0;
+      for (int i = 0; i < size; i++) {
+         // Activate NFA.
+         setactive(nfa[i%nnfa], 0, status);
+         
+      }
+      stack_clear();
+   }
 
    return 0;
 }
@@ -37,17 +53,36 @@ void
 update
 (
  nfa_t * nfa,
- int     node,
  char    value,
  int     satus
 )
 {
    int rows = nfa->rows;
-   if (nfa->exp[node/rows] & value > 0) setactive(nfa, node + rows);
-   else {
-      setactive(nfa, node + 1, status);
-      setactive(nfa, node + rows + 1, status);
+
+   // Swap stacks
+   nstack_t * active = nfa->temp;
+   nstack_t * temp = nfa->active;
+   nfa->stack = active;
+   nfa->temp  = temp;
+
+   // Update active nodes
+   for (int i = 0; i < active->p; i++) {
+      int node = stack->val[i];
+      if (nfa->exp[node/rows] & value > 0) setactive(nfa, node + rows);
+      else {
+         setactive(nfa, node + 1, status);
+         setactive(nfa, node + rows + 1, status);
+      }
    }
+
+   // Detect match. (If there are multiple matches, take the min distance match)
+   mbuf_t * buf = nfa->mbuf;
+   int tau = rows - 1;
+   int m = 0;
+   int offset = rows * (nfa->cols - 1);
+   for (; m < rows; m++) if (nfa->act[offset + m] == status) break;
+   buf->p = (buf->p + 1) % (2*tau+1);
+   buf->m[buf->p] = m;
 }
 
 
@@ -63,6 +98,45 @@ setactive
    int rows = nfa->rows;
    if (active[node] == status) return;
    active[node] = status;
+   // Do not add matches to list.
+   if (node < nfa->rows * (nfa->cols - 1)) stack_add(nfa->temp, node);
    int free = node + rows + 1;
    if (free < rows * nfa->cols) setactive(nfa, free, status);
+}
+
+
+nstack_t *
+new_stack
+(
+ int elements
+)
+{
+   nstack_t * stack = malloc(sizeof(nstack_t) + elements*sizeof(int));
+   if (stack == NULL) {
+      fprintf(stderr, "error allocating nstack_t\n");
+      exit(1);
+   }
+   stack->p = 0;
+   stack->l = elements;
+   return stack;
+}
+
+void
+stack_add
+(
+ nstack_t ** stackp,
+ int         value
+)
+{
+   nstack_t * stack = *stackp;
+   if (stack->p >= stack->l) {
+      size_t newsize = 2*stack->l;
+      *stackp = stack = realloc(stack, sizeof(nstack_t)+newsize*sizeof(int));
+      if (stack == NULL) {
+         fprintf(stderr, "error reallocating nstack\n");
+         exit(1);
+      }
+   }
+   
+   stack->val[stack->p++] = value;
 }
--- seeq.h
+++ seeq.h
@@ -1,11 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define INITIAL_LINE_SIZE 10
+#define INITIAL_STACK_SIZE 256
+
 typedef struct nfa_t nfa_t;
+typedef struct nstack_t nstack_t;
 
 struct nfa_t {
-   int    rows;
-   int    cols;
-   char * exp;
-   int  * act;
+   int        rows;
+   int        cols;
+   char     * exp;
+   int      * act;
+   nstack_t * stack;
+   nstack_t * temp;
+   mbuf_t   * mbuf;
+};
+
+struct nstack_t {
+   int p;
+   int l;
+   int val[];
+};
+
+struct mbuf_t {
+   int p;
+   int m[];
 };
